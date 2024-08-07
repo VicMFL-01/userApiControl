7ea0fc5b7e166aaa000c9022514939cd7bebeadf
VICFL <arriesgadofatal@mail.com>
2024-08-06 21:46:47 -0600

    Se modifica el componente userCard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { UserCardComponent } from "./components/user-card/user-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CardModule, ButtonModule],
+  imports: [RouterOutlet, CardModule, ButtonModule, UserCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ButtonModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css'
 })
