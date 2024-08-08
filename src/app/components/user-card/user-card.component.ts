4329aaf3d44acf639171fd0fc12fd0a573dc8ecb
VICFL <arriesgadofatal@mail.com>
2024-08-07 18:05:19 -0600

    Se agregan modificaciones a user card

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { User } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-user-card',
@@ -10,5 +11,16 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './user-card.component.css'
 })
 export class UserCardComponent {
+
+  @Input() userData:User | null = null;
+
+  userTest:User = {
+    "id": 25,
+    "first_name": "María del Carmen",
+    "last_name": "Herrera Villanueva",
+    "username": "mariadelcarmen.herrera",
+    "email": "mariadelcarmen.herreravillanueva@peticiones.online",
+    "image": "https://i.pravatar.cc/500?u=mariadelcarmen.herreravillanueva@peticiones.online"
+  }
 
 }
