8f685cde37ad3c74dbea02b7382d2db4cfcb3226
VICFL <arriesgadofatal@mail.com>
2024-08-08 14:01:11 -0600

    Se modifica el componente menubar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'Home',
+              icon: 'pi pi-home'
+          },
+          {
+              label: 'Nuevo Usuario',
+              icon: 'pi pi-user'
+          }
+      ];
+  }
 }
+
